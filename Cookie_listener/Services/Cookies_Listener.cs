0b940b1710abba45239937f2c389fc14bb94ebeb
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-07-05 16:32:26 +0300

    review

--- Cookie_listener/Services/Cookies_Listener.cs
+++ Cookie_listener/Services/Cookies_Listener.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Management;
 using System.ServiceProcess;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
